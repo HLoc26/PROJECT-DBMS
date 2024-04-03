7c9b820536ea303c378a3d9a614262a82574839c
HLoc26 <dhl26052004@gmail.com>
2024-04-03 18:28:43 +0700

    first commit

---
+++ DBMS_BookStore/DAO/DAO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBMS_BookStore.DAO
+{
+    internal class DAO
+    {
+        SqlConnection db = new SqlConnection(Properties.Settings.Default.ConnectionString);
+    }
+}
