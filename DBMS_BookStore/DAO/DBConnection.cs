66957459418d8497f956dac126e3c8d07629044c
HLoc26 <dhl26052004@gmail.com>
2024-04-10 17:52:45 +0700

    Added DBConnection.cs

---
+++ DBMS_BookStore/DAO/DBConnection.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBMS_BookStore.DAO
+{
+    internal class DBConnection
+    {
+        SqlConnection db = new SqlConnection("Data Source=LAPTOP-LDCUFKEL;Initial Catalog=Proj_DBMS_BookStore;Integrated Security=True;Trust Server Certificate=True");
+    }
+}
