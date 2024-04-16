3bf2c305bfac6974a523b8313e24bcc45d03de28
PeoPeo-1712 <baonguyenhuynhquocbl03@gmail.com>
2024-04-17 00:52:52 +0700

    finish DTO THeTV, KhachHang

--- DBMS_BookStore/DTO/KhachHang.cs
+++ DBMS_BookStore/DTO/KhachHang.cs
@@ -1,8 +1,46 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-public class Class1
+namespace DBMS_BookStore.DTO
 {
-	public Class1()
-	{
-	}
+    internal class KhachHang
+    {
+        string maKH;
+        string ho;
+        string tenLot;
+        string ten;
+        DateTime ngaySinh;
+        string gioiTinh;
+
+        public KhachHang(string maKH, string ho, string tenLot, string ten, DateTime ngaySinh, string gioiTinh)
+        {
+            MaKH = maKH;
+            Ho = ho;
+            TenLot = tenLot;
+            Ten = ten;
+            NgaySinh = ngaySinh;
+            GioiTinh = gioiTinh;
+        }
+
+        public KhachHang(DataRow dr)
+        {
+            MaKH = dr["MaKH"].ToString();
+            Ho = dr["Ho"].ToString();
+            TenLot = dr["TenLot"].ToString();
+            Ten = dr["Ten"].ToString();
+            NgaySinh = (DateTime)dr["ngaySinh"];
+            GioiTinh = dr["GioiTinh"].ToString();
+
+        }
+        public string MaKH { get => maKH; set => maKH = value; }
+        public string Ho { get => ho; set => ho = value; }
+        public string TenLot { get => tenLot; set => tenLot = value; }
+        public string Ten { get => ten; set => ten = value; }
+        public DateTime NgaySinh { get => ngaySinh; set => ngaySinh = value; }
+        public string GioiTinh { get => gioiTinh; set => gioiTinh = value; }
+    }
 }
--- DBMS_BookStore/DTO/TheTV.cs
+++ DBMS_BookStore/DTO/TheTV.cs
@@ -1,8 +1,47 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-public class Class1
+namespace DBMS_BookStore.DTO
 {
-	public Class1()
+    internal class TheTV
 	{
-	}
+        string maThe;
+        int soDiem;
+        string tenBacTV;
+        DateTime ngayBatDau;
+        DateTime ngayKetThuc;
+        string maKH;
+
+        public TheTV(string maThe, int soDiem, string tenBacTV, DateTime ngayBatDau, DateTime ngayKetThuc, string maKH)
+        {
+            MaThe = maThe;
+            SoDiem = 0;
+            TenBacTV = tenBacTV;
+            NgayBatDau = ngayBatDau;
+            NgayKetThuc = ngayKetThuc;
+            MaKH = maKH;
+        }
+        public TheTV(DataRow dr)
+        {
+            MaThe = dr["MaThe"].ToString();
+            SoDiem = (int)dr["SoDiem"];
+            TenBacTV = dr["TenBacTV"].ToString();
+            NgayBatDau = (DateTime)dr["NgayBatDau"];
+            NgayKetThuc = (DateTime)dr["NgayKetThuc"];
+            MaKH = dr["MaKH"].ToString();
+
+        }
+        public string MaThe { get => maThe; set => maThe = value; }
+        public int SoDiem { get => soDiem; set => soDiem = value; }
+        public string TenBacTV { get => tenBacTV; set => tenBacTV = value; }
+        public DateTime NgayBatDau { get => ngayBatDau; set => ngayBatDau = value; }
+        public DateTime NgayKetThuc { get => ngayKetThuc; set => ngayKetThuc = value; }
+        public string MaKH { get => maKH; set => maKH = value; }
+    }
+
 }
